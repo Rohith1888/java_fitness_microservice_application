dd0b81ee438b7b28016b04f4b454f6481327ebf9
Rohith1888 <gopisettyrohith18@gmail.com>
2026-01-21 22:29:58 +0530

    Revert "added end points in the activity service"
    This reverts commit ed63dc7c60ff6b86c7a41691f68dcf7a89ec0df2.

--- activityservice/src/main/java/com/fitness/activityservice/service/ActivityService.java
+++ activityservice/src/main/java/com/fitness/activityservice/service/ActivityService.java
@@ -8,9 +8,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 @Service
 //@RequiredArgsConstructor
 public class ActivityService {
@@ -46,21 +43,5 @@ public class ActivityService {
         response.setCreatedAt(activity.getCreatedAt());
         response.setUpdatedAt(activity.getUpdatedAt());
         return response;
-    }
-
-    public List<ActivityResponse> getUserActivities(String userId) {
-       List<Activity> activities = activityRepository.findByUserId(userId);
-
-       return activities.stream()
-               .map(this::mapToResponse)
-               .collect(Collectors.toList());
-
-    }
-
-    public ActivityResponse getActivityById(String activityId) {
-        return  activityRepository.findById(activityId)
-                .map(this::mapToResponse)
-                .orElseThrow(()->
-                        new RuntimeException("Activiy Not found with Id"+ activityId));
     }
 }
